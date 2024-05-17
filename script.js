014e4a6656168b051bc5ef81da17bd601addb710
Daniël van Ginneken <dcjvanginneken@gmail.com>
2024-05-18 00:46:14 +0200

    Added Dark mode

--- script.js
+++ script.js
@@ -5,7 +5,49 @@ function ToggleMenu() {
   icon.classList.toggle("open");
 }
 
-function BackTopFunction() {
-  document.body.scrollTop = 1; // For Safari
-  document.documentElement.scrollTop = 1; // For Chrome, Firefox, IE and Opera
+// Dark / light mode
+
+const btn = document.getElementById("modeToggle");
+const btn2 = document.getElementById("modeToggle2");
+const themeIcons = document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+  setDarkMode();
+}
+
+btn.addEventListener("click", function () {
+  setTheme();
+});
+
+btn2.addEventListener("click", function () {
+  setTheme();
+});
+
+function setTheme() {
+  let currentTheme = document.body.getAttribute("theme");
+
+  if (currentTheme === "dark") {
+    setLightMode();
+  } else {
+    setDarkMode();
+  }
+}
+
+function setDarkMode() {
+  document.body.setAttribute("theme", "dark");
+  localStorage.setItem("theme", "dark");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-dark");
+  });
+}
+
+function setLightMode() {
+  document.body.removeAttribute("theme");
+  localStorage.setItem("theme", "light");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-light");
+  });
 }
