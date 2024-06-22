60934f3e09fedffed757cfa3114f8f3de7cc2800
Daniël van Ginneken <dcjvanginneken@gmail.com>
2024-06-22 23:10:45 +0200

    Added Cookies popup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # website-uk
 
-My website (in code) that is hosted under the danielvanginneken.co.uk domain name.
+My website (in code) that is hosted under the danielvanginneken.nl domain name.
 Now with personal information added to it.
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function ToggleMenu() {
   icon.classList.toggle("open");
 }
 
-// Function to calculate age based on birthdate
+/// Function to calculate age based on birthdate
 function calculateAge(birthDate) {
   const today = new Date();
   const birthDateObj = new Date(birthDate);
@@ -77,3 +77,50 @@ function setLightMode() {
     icon.src = icon.getAttribute("src-light");
   });
 }
+
+/// Function for cookie handling
+function setCookie(cName, cValue, expDays) {
+  let date = new Date();
+  date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = cName + "=" + cValue + ";" + expires + "; path=/";
+}
+
+function getCookie(cName) {
+  const name = cName + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split("; ");
+
+  for (let cookie of cookieArray) {
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length);
+    }
+  }
+
+  return null;
+}
+
+document.querySelector("#cookies-accept-btn").addEventListener("click", () => {
+  const cookiesBanner = document.querySelector("#cookies");
+  cookiesBanner.classList.add("fade-out");
+  setTimeout(() => {
+    cookiesBanner.style.display = "none";
+  }, 1000); // Match the duration of the animation
+  setCookie("cookie", true, 15);
+});
+
+cookieMessage = () => {
+  if (!getCookie("cookie"))
+    document.querySelector("#cookies").style.display = "block";
+};
+
+window.addEventListener("load", cookieMessage);
+
+/// HTML file extension removed from URL
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentUrl = window.location.pathname;
+  if (currentUrl.endsWith("/information")) {
+    window.location.replace(currentUrl + ".html");
+  }
+});
